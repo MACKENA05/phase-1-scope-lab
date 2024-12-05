b5835d1ccb3ea88569238612719ca40e6f920e63
MonicahMackena¨ <mackenamuthui@gmail.com>
2024-12-05 18:58:43 +0300

    updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName() {
+   console.log(customerName = customerName.toUpperCase());  
+}
+
+function setBestCustomer(){
+     bestCustomer = 'not bob'
+    console.log(bestCustomer);
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+    console.log(bestCustomer);
+}
+
+const leastFavouriteCustomer = 'bob';
+
+function changeLeastFavoriteCustomer(){
+    leastFavouriteCustomer = 'ann'
+    console.log(leastFavouriteCustomer)
+}
